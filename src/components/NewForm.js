4b98e215df59a3a8d13fff1b53f86e1778335c36
davidjrada <davidjrada@gmail.com>
2019-05-13 14:22:26 -0400

    updated fetch address to heroku backend

--- src/components/NewForm.js
+++ src/components/NewForm.js
@@ -7,7 +7,7 @@ let baseURL = process.env.REACT_APP_BASEURL
 if (process.env.NODE_ENV === 'development') {
     baseURL = 'http://localhost:3004'
 } else {
-    baseURL = 'https://fathomless-sierra-68956.herokuapp.com'
+    baseURL = 'https://gitpub-backend.herokuapp.com'
 }
 
 console.log('current base URL:', baseURL)

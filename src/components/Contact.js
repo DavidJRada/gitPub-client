eb4b998540059b828948d3903e6a7fb3f42b5796
davidjrada <davidjrada@gmail.com>
2019-05-14 14:10:06 -0400

    added contact stuff

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import 'materialize-css'; // It installs the JS asset only
+import 'materialize-css/dist/css/materialize.min.css';
+
 
 class Contact extends React.Component {
     render() {
         return (
             <div className='contact'>
-                <h1>Contact</h1>
+                <h1>Contact Us</h1>
+                <h3>Want to stay up to date on our grand opening and new specials? </h3>    
+                <div className=''></div>
             </div>
         )
     }
